43ceac5c21dabc3d039dfaba34784eae8910ff18
Cezar Drumea <cdrumea.griddynamics@gmail.com>
2024-11-07 11:07:00 +0200

    Refactor

--- utils/validateDate.js
+++ utils/validateDate.js
@@ -10,10 +10,15 @@ export default function (date, res) {
 
   const [year, month, day] = date.split('-').map((s) => parseInt(s));
 
-  if (month < 1 || month > 12 || day < 1 || day > 31) {
+  if (day < 1 || day > 31) {
     res.status(400).json({
-      error:
-        'Invalid date, month should be between 1 and 12 and day should be between 1 and 31',
+      error: 'Invalid date, day should be between 1 and 31',
+    });
+  }
+
+  if (month < 1 || month > 12) {
+    res.status(400).json({
+      error: 'Invalid date, month should be between 1 and 12',
     });
     return true;
   }
